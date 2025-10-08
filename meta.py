01c3008c936fed21b009ec45549f2ed1e2c13345
iyer-atomic <65344441+iyer-atomic@users.noreply.github.com>
2025-10-08 14:55:17 +0530

    Update meta.py

--- meta.py
+++ meta.py
@@ -1,22 +1,13 @@
-# Copyright 2019 TerraPower, LLC
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-"""
-Metadata describing an ARMI distribution.
-"""
-
-# duplicating with setup.py for now. This is because in order to import meta.py, we
-# need to run armi.__init__, which does a whole heck of a lot of stuff that setup.py
-# shouldn't need. We should clean this up in the future.
-__version__ = "0.1.7"
+{
+  "type": "service_account",
+  "project_id": "alert-nimbus-335411",
+  "private_key_id": "7ea337b9cb965a19fbf4bd0ed1ee694294b2b47d",
+  "private_key": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQDh13ZkfDcRo3Ja\n3TbcKDwOUeFITdAnd+hJlVJ80A9H3B/f5tzL7F11tgYk5fBsZmeKWuR5ZhZRzi06\ng84pjI+dSjJnEkhw6JyzL5oZtF4DDMow3X9AgXk6HoqAAaugKEsb+BbVZEqBlzdq\nnMOi6FUFCQzTQmO9UahgN13ccZyUcIDHA3Fy605UGJiTjoWrX6dK8PpzCK/B9L3J\naBziaLYAEf3SYZdrmotvr2GFcaF/WKCrEnc81d0vrvKwa2XRCutuhJapKhXRxWKt\njA88rQ48Bangq8KLgIYK6QCbmhOL8i0heXYZHUziLduAlJl0LTIQH75kcPQdKfHi\nap22O5IfAgMBAAECggEAA8+Fn5zQLEqEdS0wcvlhIRzhQ+Ur6mLg1Io/gUEXx66p\nWfInAnAoST2FI47v/FEtkE1Wsrk600H/6XHilq8dq4sFr4qJoCUqynojBu6ymRW6\n+BtqM5qgloUxU5rpw6BkRPht726rb1F+cojhAKVXKF5miAmOAYHOt+zkxdB8efw5\nPsVJGQdtSzM0i6jBJjTUM555eBd95vdfV6k2V+gmNTHXbT/DHWjbiCWAYcQkhXBy\n5/5OqtzxbAZJYfROLOTG6OaIJg7KBZcTKBbMN3JU72o3kxbKeW6dbhvaqGrhdqFp\n/AK0FwXqhzJIQxIFoh4wqxepwq9hBmY+h4uMZ8VQwQKBgQD3vV+RbWr6GJAM5Xjo\nme1Fx3W5cFCUD2ZTZGo3XRdB5ZT8PKIy2XxhLbmOqfKii/bGd7sow66SVgUbwMSk\nDHTHS8Hlswzozbp7jvO7Lh2c7QqnpAFpwkb/HtCgJKnuwxYDxGHPgHe7CTk86KN8\nNwB1r047l/P/WsUfVvkSu0ST3wKBgQDpXyyS2T4UWjCx6ONysamG8L8lBxdY4L/M\nEq3ZJva6/3iPycZTMeFmUIBxrdkpsqXcjkFY+egx2KdL9aUSG4TMDTMVnlx7oXqu\nL0z5VJspNXY+OU3a1NCdrr4wOxpXPb63TXU6DFFVpd9ZNjUsjqAQ39sQOuSBhWtC\nO3eb7lLJwQKBgDjuz0ingZ8qB+i+z5N81Fdl6DsOgaYVB+oAOJyyiwa80QlKkefn\nrTrT/EG58qXCTpr7pwpONg8xL4laOpaMukWWtl+oTz4tET8V8hNBHNMUco9mFGEO\noaw9k6pJ4reZI7UH64Z5OorEKuBBgliodzWUbQErAYrcfFKgdSsF2FO3AoGBALfv\ngaxEeX+FDnhK/Gn7yYhH3AANNMOicMkht9A9u53rmVSLTjtoRyVjMblnspAA9GV/\n4dWFMxOGlWVzOSWqduJ1TNwvMulmftEnCt5BZwH11zp34/skXUDK57AQBVTZn6XN\n8SVNbBW7ZOxlo0d5KoPiTEDgihXj2IQdZsqJ7sqBAoGAT+kVvagk/YgeZU7dTLCI\nJgi++lz52n+Y3GG4X800BPVrtXyDIF/1Q0ChN9U5jo09sUB+gMW/wxUCgx3e3FZ6\nMlZY0oQUH7z3goj8kASSU2KzHgVSKFNd8vDMV3wdMdl1jDNCoXlloBML4JNq5+0B\nsM1TmzQ+TxUTtS53Z2WwAHo=\n-----END PRIVATE KEY-----\n",
+  "client_email": "dev-user-service-account@alert-nimbus-335411.iam.gserviceaccount.com",
+  "client_id": "106429067703827180960",
+  "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+  "token_uri": "https://oauth2.googleapis.com/token",
+  "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+  "client_x509_cert_url": "https://www.googleapis.com/robot/v1/metadata/x509/dev-user-service-account%40alert-nimbus-335411.iam.gserviceaccount.com",
+  "universe_domain": "googleapis.com"
+}
